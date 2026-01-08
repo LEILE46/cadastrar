1a609a2fd9bf3df5bf1da1943876aaca235ca6fd
LEILE46 <leile.bambil2705@gmail.com>
2026-01-08 17:33:47 -0400

    arrumando models

--- meuapp/models.py
+++ meuapp/models.py
@@ -1,18 +1,25 @@
+from django.db import models
 from django.shortcuts import render
-from .models import Usuario
-from django.contrib.auth.hashers import make_password  # para senha segura
+from django.contrib.auth.hashers import make_password 
+class Usuario(models.Model):
+    nome = models.CharField(max_length=100)
+    senha = models.CharField(max_length=128)
+
+    def __str__(self):
+        return f"usuario = {self.nome}"
+
 
 def cadastro(request):
     if request.method == 'POST':
         nome = request.POST.get('user')
         senha = request.POST.get('senha')
 
-        if nome and senha:  
+        if nome and senha:  # checa se os campos não estão vazios
             senha_hash = make_password(senha)
             Usuario.objects.create(nome=nome, senha=senha_hash)
             return render(request, 'meuapp/index.html', {
                 'nome_cadastrado': nome
             })
 
-
+    # Se não for POST ou campos vazios
     return render(request, 'meuapp/index.html')
